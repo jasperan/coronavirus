ab4b1fda505d4b9b990bfbfa8794fce5ade8a940
Ignacio Guillermo Martinez <23caj23@gmail.com>
2020-03-31 14:27:34 +0200

    .

--- parse.py
+++ parse.py
@@ -2,7 +2,9 @@ import requests
 from sys import exit
 import json
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
