f2adc71445e39065e95e95f90c1248e9201c0170
Ignacio Guillermo Martinez <23caj23@gmail.com>
2020-03-31 14:29:47 +0200

    .

--- parse.py
+++ parse.py
@@ -5,6 +5,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
 def hello_world():
